ba4072978a658296c844fb27f470b0b5191d4030
Byungkun Lee <byungkun.lee@kaist.ac.kr>
2022-05-14 22:49:07 -0700

    9 수정

--- run_edl_instance_9.py
+++ run_edl_instance_9.py
@@ -17,7 +17,7 @@ def run_experiment(experiment):
 
 
 if __name__ == '__main__':
-    default = ['maximum_timestep=50', 'use_wandb=true', 'agent.batch_size=64', 
+    default = ['maximum_timestep=200', 'use_wandb=true', 'agent.batch_size=64', 
     'agent.init_alpha=0.2', 'agent.feature_dim=128','agent.hidden_dim=128',
     'num_pretrain_frames=510000','oracle_dur=50000', 'num_train_frames=2010000']
     seeds = ['seed=100', 'seed=101','seed=102', 'seed=103','seed=104','seed=105']
